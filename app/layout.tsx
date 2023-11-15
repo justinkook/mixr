cac5e057b94d0137bb8bd20af9d0535c1124fefc
justinkook <justinkook@gmail.com>
2023-11-14 21:49:06 -0500

    update middleware to initial and add clerk appearance

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,7 +93,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "bg-primary",
+        },
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
