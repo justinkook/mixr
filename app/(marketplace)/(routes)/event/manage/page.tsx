6996f2983b1d974dc12d69cdd3c2fdf28602c712
justinkook <justinkook@gmail.com>
2023-11-16 23:43:11 -0500

    add clerk to next image and move overview content to analytics, move manage to event/manage

--- app/(marketplace)/(routes)/event/manage/page.tsx
+++ app/(marketplace)/(routes)/event/manage/page.tsx
@@ -61,9 +61,8 @@ export default async function EventManagePage() {
                 <TabsTrigger value="overview">Overview</TabsTrigger>
                 <TabsTrigger value="guests">Guests</TabsTrigger>
                 <TabsTrigger value="tickets">Tickets</TabsTrigger>
-                <TabsTrigger value="sponsors">Sponsors</TabsTrigger>
-                <TabsTrigger value="analytics">Analytics</TabsTrigger>
-                <TabsTrigger value="settings">Settings</TabsTrigger>
+                <TabsTrigger value="insights">Insights</TabsTrigger>
+                <TabsTrigger value="settings">More</TabsTrigger>
               </TabsList>
               <TabsContent value="overview" className="space-y-4">
                 <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
@@ -235,42 +234,132 @@ export default async function EventManagePage() {
                   <Separator />
                 </div>
               </TabsContent>
-              <TabsContent value="sponsors" className="space-y-4">
-                <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-                  <div className="flex items-center justify-between space-y-2">
-                    <div>
-                      <h2 className="text-2xl font-bold tracking-tight">
-                        Sponsors
-                      </h2>
-                      <p className="text-muted-foreground">
-                        Create sponsor tickets and outline terms
+              <TabsContent value="insights" className="space-y-4">
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Total Revenue
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">$45,231.89</div>
+                      <p className="text-xs text-muted-foreground">
+                        $120.13 from sponsors
                       </p>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <Button variant="secondary">
-                        <Plus size={20} />
-                        Create Sponsor
-                      </Button>
-                    </div>
-                  </div>
-                  <Separator />
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Guests
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                        <circle cx="9" cy="7" r="4" />
+                        <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">50 going</div>
+                      <p className="text-xs text-muted-foreground">
+                        180 guests invited
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Impressions
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <rect width="20" height="14" x="2" y="5" rx="2" />
+                        <path d="M2 10h20" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">234 viewed</div>
+                      <p className="text-xs text-muted-foreground">
+                        119 clicks
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Active Now
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">+573</div>
+                      <p className="text-xs text-muted-foreground">
+                        +201 since last hour
+                      </p>
+                    </CardContent>
+                  </Card>
                 </div>
-              </TabsContent>
-              <TabsContent value="analytics" className="space-y-4">
-                <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
-                  <div className="flex items-center justify-between space-y-2">
-                    <div>
-                      <h2 className="text-2xl font-bold tracking-tight">
-                        Analytics
-                      </h2>
-                      <p className="text-muted-foreground">
-                        See active views, device, or location breakdown
-                      </p>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                      <Button variant="secondary">Enable</Button>
-                    </div>
-                  </div>
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                  <Card className="col-span-4">
+                    <CardHeader>
+                      <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                      <Overview />
+                    </CardContent>
+                  </Card>
+                  <Card className="col-span-3">
+                    <CardHeader>
+                      <CardTitle>Recent Guests</CardTitle>
+                      <CardDescription>
+                        You have 265 guests attending.
+                      </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <RecentSales />
+                    </CardContent>
+                  </Card>
                 </div>
               </TabsContent>
               <TabsContent value="settings" className="space-y-4">
--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ const nextConfig = withPWA({
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
     ],
   },
 });
