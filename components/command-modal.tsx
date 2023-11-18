4cf833aac8fb3f3e3155456c41bdbf4c85f7a041
justinkook <justinkook@gmail.com>
2023-11-17 23:00:44 -0500

    feat: input functionality command modal

--- components/command-modal.tsx
+++ components/command-modal.tsx
@@ -1,12 +1,8 @@
-import {
-  Calculator,
-  Calendar,
-  CreditCard,
-  Settings,
-  Smile,
-  User,
-} from "lucide-react";
+"use client";
 
+import { useState } from "react";
+
+import { Calendar, CreditCard, Settings, Smile, User } from "lucide-react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -24,24 +20,25 @@ type CommandModalProps = {
 };
 
 export function CommandModal({ open, onOpenChange }: CommandModalProps) {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
     <>
       <CommandDialog open={open} onOpenChange={onOpenChange}>
-        <CommandInput placeholder="Type a command or search..." />
+        <CommandInput
+          placeholder="Type a command or search..."
+          value={searchQuery}
+          onValueChange={(q) => setSearchQuery(q)}
+        />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
           <CommandGroup heading="Suggestions">
             <CommandItem>
               <Calendar className="mr-2 h-4 w-4" />
-              <span>Calendar</span>
+              <span>{`Search ${searchQuery} in Events`}</span>
             </CommandItem>
             <CommandItem>
               <Smile className="mr-2 h-4 w-4" />
-              <span>Search Emoji</span>
-            </CommandItem>
-            <CommandItem>
-              <Calculator className="mr-2 h-4 w-4" />
-              <span>Calculator</span>
+              <span>{`Search ${searchQuery} in Organizers`}</span>
             </CommandItem>
           </CommandGroup>
           <CommandSeparator />
