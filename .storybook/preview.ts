748cf98900292a552205d08158fff2261581cd64
justinkook <justinkook@gmail.com>
2023-11-18 20:56:04 -0500

    feat(storybook): remove passWithNoTests and add stories to shadcn ui components

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,9 @@ import "../styles/globals.css"
 
 const preview: Preview = {
   parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
