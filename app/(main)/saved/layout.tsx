5da0d556d83128cde34fff8de5bcd0b1e0be3542
justinkook <justinkook@gmail.com>
2023-12-14 08:36:13 -0500

    fix(typescript): navConfig import in saved layouts

--- app/(main)/saved/layout.tsx
+++ app/(main)/saved/layout.tsx
@@ -1,4 +1,4 @@
-import { navConfig } from '@/config/nav'
+import { userNav } from '@/config/nav'
 
 import TabNav from '@/components/event/manage/tab-nav'
 
@@ -8,7 +8,7 @@ export default function SavedLayout({ children }: { children: React.ReactNode })
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Saved Events</h2>
       </div>
-      <TabNav items={navConfig.userNav.find((navItem) => navItem.title === 'Saved')?.items!} />
+      <TabNav items={userNav.find((navItem) => navItem.title === 'Saved')?.items!} />
       {children}
     </div>
   )
