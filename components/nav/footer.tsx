d71af6f77d535c86e2b074e675d3b3ffa307e836
justinkook <justinkook@gmail.com>
2023-12-16 03:31:46 -0500

    fix(typescript): href to terms and privacy footer

--- components/nav/footer.tsx
+++ components/nav/footer.tsx
@@ -8,12 +8,12 @@ export function Footer() {
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8">
         <p className="text-sm leading-7 text-muted-foreground">&copy; 2023 Revent. All rights reserved.</p>
         <div className="hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-muted" />
-        <Link href="/terms-conditions" className="text-muted-foreground hover:text-muted-foreground">
+        <Link href="/terms" className="text-muted-foreground hover:text-muted-foreground">
           <span className="sr-only">Terms & Conditions</span>
           <p className="text-sm leading-7 text-muted-foreground">Terms</p>
         </Link>
         <div className="hidden sm:block sm:h-7 sm:w-px sm:flex-none sm:bg-muted" />
-        <Link href="/privacy-policy" className="text-muted-foreground hover:text-muted-foreground">
+        <Link href="/privacy" className="text-muted-foreground hover:text-muted-foreground">
           <span className="sr-only">Privacy Policy</span>
           <p className="text-sm leading-7 text-muted-foreground">Privacy</p>
         </Link>
