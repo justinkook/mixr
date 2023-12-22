54c09cb95ad19138684c0bfa2a0ee71319cf67fa
justinkook <justinkook@gmail.com>
2023-12-21 21:56:44 -0500

    refactor(dashboard): change subscriptions to guests in overview dashbaord

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -36,11 +36,11 @@ export default async function DashboardPage() {
         </Card>
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
+            <CardTitle className="text-sm font-medium">Guests</CardTitle>
             <UsersIcon className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
+            <div className="text-2xl font-bold">235 unique</div>
             <p className="text-xs text-muted-foreground">
               +180.1% from last month
             </p>
@@ -52,7 +52,7 @@ export default async function DashboardPage() {
             <CreditCardIcon className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
+            <div className="text-2xl font-bold">$12,234</div>
             <p className="text-xs text-muted-foreground">
               +19% from last month
             </p>
@@ -66,7 +66,7 @@ export default async function DashboardPage() {
             <ActivityIcon className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+573</div>
+            <div className="text-2xl font-bold">+573 views</div>
             <p className="text-xs text-muted-foreground">+201 clicks</p>
           </CardContent>
         </Card>
