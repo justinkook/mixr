f85deee0b1791fb8bad5d1ab1976f053753f3da9
justinkook <justinkook@gmail.com>
2024-01-11 20:43:42 -0500

    fix(landing-page): fix link get started

--- app/page.tsx
+++ app/page.tsx
@@ -215,7 +215,7 @@ export default function LandingPage() {
                   </div>
                   <div className="py-6">
                     <a
-                      href="https://app.gen3tickets.com"
+                      href="https://reventapp.com"
                       className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                     >
                       Get Started
