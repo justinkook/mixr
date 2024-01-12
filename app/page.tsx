c994162add3a33c933f1d22dc43bfb9444a6f444
justinkook <justinkook@gmail.com>
2024-01-11 20:29:59 -0500

    fix(landing): remove div wrapper dialog trigger

--- app/page.tsx
+++ app/page.tsx
@@ -177,66 +177,64 @@ export default function LandingPage() {
               />
             </a>
           </div>
-          <div className="flex lg:hidden">
-            <Dialog>
-              <DialogTrigger asChild>
-                <button
-                  type="button"
-                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </DialogTrigger>
-              <div className="lg:hidden fixed inset-0 z-50" />
-              <DialogContent className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                <div className="flex items-center justify-between">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Revent</span>
-                    <Image
-                      className="h-4 w-auto"
-                      src="/images/logo-white.svg"
-                      alt="Revent Logo"
-                      height={32}
-                      width={128}
-                    />
-                  </a>
-                  <DialogClose asChild>
-                    <button
-                      type="button"
-                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </DialogClose>
-                </div>
-                <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
-                    <div className="space-y-2 py-6">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          {item.name}
-                        </a>
-                      ))}
-                    </div>
-                    <div className="py-6">
+          <Dialog>
+            <DialogTrigger asChild>
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400 lg:hidden"
+              >
+                <span className="sr-only">Open main menu</span>
+                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </DialogTrigger>
+            <div className="lg:hidden fixed inset-0 z-50" />
+            <DialogContent className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Revent</span>
+                  <Image
+                    className="h-4 w-auto"
+                    src="/images/logo-white.svg"
+                    alt="Revent Logo"
+                    height={32}
+                    width={128}
+                  />
+                </a>
+                <DialogClose asChild>
+                  <button
+                    type="button"
+                    className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </DialogClose>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
                       <a
-                        href="https://app.gen3tickets.com"
-                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                       >
-                        Get Started
+                        {item.name}
                       </a>
-                    </div>
+                    ))}
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="https://app.gen3tickets.com"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      Get Started
+                    </a>
                   </div>
                 </div>
-              </DialogContent>
-            </Dialog>
-          </div>
+              </div>
+            </DialogContent>
+          </Dialog>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
               <a
