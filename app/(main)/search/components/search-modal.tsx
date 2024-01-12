b51a949d442fea772e97641137a7aa0fc65bc268
justinkook <justinkook@gmail.com>
2024-01-11 21:15:35 -0500

    fix(search-button): fix full width mobile search button

--- app/(main)/search/components/search-modal.tsx
+++ app/(main)/search/components/search-modal.tsx
@@ -133,191 +133,191 @@ export const SearchModal = () => {
   }, [step])
 
   return (
-    <div className="flex justify-center w-full py-4">
-      <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger>
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <div className="flex justify-center w-full py-4">
           <SearchButton />
-        </SheetTrigger>
-        <SheetContent className="w-full h-full flex items-center flex-col sm:min-w-[500px]">
-          <SheetHeader>
-            <SheetTitle>Search</SheetTitle>
-          </SheetHeader>
-          <Accordion
-            type="single"
-            collapsible
-            className="w-full"
-            defaultValue="where"
-            value={
-              step === STEPS.LOCATION
-                ? 'where'
-                : step === STEPS.DATE
-                  ? 'when'
-                  : 'what'
-            }
-          >
-            <AccordionItem value="where">
-              <AccordionTrigger onClick={() => setStep(STEPS.LOCATION)}>
-                Where
-              </AccordionTrigger>
-              <AccordionContent>
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Where do you wanna go?</CardTitle>
-                    <CardDescription>
-                      Find the perfect location for your next event
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Command>
-                      <CommandInput
-                        placeholder="Anywhere"
-                        value={location}
-                        onValueChange={setLocation}
-                        className="w-full"
-                      />
-                      <CommandEmpty>No place found</CommandEmpty>
-                      <CommandGroup className="min-h-[200px]">
-                        {data &&
-                          data.predictions?.length > 0 &&
-                          data.predictions.map(
-                            (prediction: any, index: number) => (
-                              <CommandItem
-                                key={index}
-                                value={prediction.description}
-                                onSelect={(currentValue) => {
-                                  setLocation(
-                                    currentValue ===
-                                      prediction.description.toLowerCase() &&
-                                      prediction.description
-                                  )
-                                }}
-                              >
-                                <MapPinIcon
-                                  className={cn(
-                                    'mr-2 h-4 w-4',
-                                    location === prediction.description
-                                      ? 'opacity-100'
-                                      : 'opacity-50'
-                                  )}
-                                />
-                                {prediction.description}
-                              </CommandItem>
-                            )
-                          )}
-                      </CommandGroup>
-                    </Command>
-                  </CardContent>
-                </Card>
-              </AccordionContent>
-            </AccordionItem>
-
-            <AccordionItem value="when">
-              <AccordionTrigger onClick={() => setStep(STEPS.DATE)}>
-                When
-              </AccordionTrigger>
-              <AccordionContent>
-                <Card className="flex justify-center w-full">
-                  <CardContent>
-                    <Calendar
-                      classNames={{
-                        caption_label: 'text-xl',
-                        head_cell: 'w-[3rem]',
-                        cell: 'w-[3rem]',
-                        day: 'w-[3rem] h-[3rem]',
-                      }}
-                      initialFocus
-                      mode="range"
-                      defaultMonth={dateRange?.from}
-                      selected={dateRange}
-                      onSelect={setDateRange}
-                      disabled={{ before: new Date() }}
+        </div>
+      </SheetTrigger>
+      <SheetContent className="w-full h-full flex items-center flex-col sm:min-w-[500px]">
+        <SheetHeader>
+          <SheetTitle>Search</SheetTitle>
+        </SheetHeader>
+        <Accordion
+          type="single"
+          collapsible
+          className="w-full"
+          defaultValue="where"
+          value={
+            step === STEPS.LOCATION
+              ? 'where'
+              : step === STEPS.DATE
+                ? 'when'
+                : 'what'
+          }
+        >
+          <AccordionItem value="where">
+            <AccordionTrigger onClick={() => setStep(STEPS.LOCATION)}>
+              Where
+            </AccordionTrigger>
+            <AccordionContent>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Where do you wanna go?</CardTitle>
+                  <CardDescription>
+                    Find the perfect location for your next event
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Command>
+                    <CommandInput
+                      placeholder="Anywhere"
+                      value={location}
+                      onValueChange={setLocation}
+                      className="w-full"
                     />
-                  </CardContent>
-                </Card>
-              </AccordionContent>
-            </AccordionItem>
+                    <CommandEmpty>No place found</CommandEmpty>
+                    <CommandGroup className="min-h-[200px]">
+                      {data &&
+                        data.predictions?.length > 0 &&
+                        data.predictions.map(
+                          (prediction: any, index: number) => (
+                            <CommandItem
+                              key={index}
+                              value={prediction.description}
+                              onSelect={(currentValue) => {
+                                setLocation(
+                                  currentValue ===
+                                    prediction.description.toLowerCase() &&
+                                    prediction.description
+                                )
+                              }}
+                            >
+                              <MapPinIcon
+                                className={cn(
+                                  'mr-2 h-4 w-4',
+                                  location === prediction.description
+                                    ? 'opacity-100'
+                                    : 'opacity-50'
+                                )}
+                              />
+                              {prediction.description}
+                            </CommandItem>
+                          )
+                        )}
+                    </CommandGroup>
+                  </Command>
+                </CardContent>
+              </Card>
+            </AccordionContent>
+          </AccordionItem>
 
-            <AccordionItem value="what">
-              <AccordionTrigger onClick={() => setStep(STEPS.INPUT)}>
-                What
-              </AccordionTrigger>
-              <AccordionContent>
-                <Card>
-                  <CardHeader>
-                    <CardTitle>Search terms</CardTitle>
-                    <CardDescription>Ask anything you want</CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Input
-                      placeholder="Search..."
-                      value={input}
-                      onChange={(v) => setInput(v.target.value)}
-                    />
-                    <Separator className="my-4" />
-                    <div className="grid gap-4">
-                      <h4 className="text-sm font-medium">Suggestions</h4>
-                      <Button
-                        variant="secondary"
-                        className="rounded-full w-fit"
-                        onClick={() =>
-                          setInput('Show me the most popular events')
-                        }
-                      >
-                        Show me the most popular events
-                      </Button>
-                      <Button
-                        variant="secondary"
-                        className="rounded-full w-fit"
-                        onClick={() =>
-                          setInput('Which event has the most attendees?')
-                        }
-                      >
-                        Which event has the most attendees?
-                      </Button>
-                      <Button
-                        variant="secondary"
-                        className="rounded-full w-fit"
-                        onClick={() => setInput(`New Year's Eve Parties`)}
-                      >
-                        New Years Eve Parties
-                      </Button>
-                      <Button
-                        variant="secondary"
-                        className="rounded-full w-fit"
-                        onClick={() => setInput('Running clubs')}
-                      >
-                        Running clubs
-                      </Button>
-                    </div>
-                  </CardContent>
-                </Card>
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
+          <AccordionItem value="when">
+            <AccordionTrigger onClick={() => setStep(STEPS.DATE)}>
+              When
+            </AccordionTrigger>
+            <AccordionContent>
+              <Card className="flex justify-center w-full">
+                <CardContent>
+                  <Calendar
+                    classNames={{
+                      caption_label: 'text-xl',
+                      head_cell: 'w-[3rem]',
+                      cell: 'w-[3rem]',
+                      day: 'w-[3rem] h-[3rem]',
+                    }}
+                    initialFocus
+                    mode="range"
+                    defaultMonth={dateRange?.from}
+                    selected={dateRange}
+                    onSelect={setDateRange}
+                    disabled={{ before: new Date() }}
+                  />
+                </CardContent>
+              </Card>
+            </AccordionContent>
+          </AccordionItem>
 
-          <SheetFooter className="absolute bottom-0 flex flex-row w-full p-6 space-x-2">
-            {secondaryActionLabel && (
-              <Button
-                variant="outline"
-                type="button"
-                className="w-[50%]"
-                size="lg"
-                onClick={step === STEPS.INPUT ? clearAll : onNext}
-              >
-                {secondaryActionLabel}
-              </Button>
-            )}
+          <AccordionItem value="what">
+            <AccordionTrigger onClick={() => setStep(STEPS.INPUT)}>
+              What
+            </AccordionTrigger>
+            <AccordionContent>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Search terms</CardTitle>
+                  <CardDescription>Ask anything you want</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <Input
+                    placeholder="Search..."
+                    value={input}
+                    onChange={(v) => setInput(v.target.value)}
+                  />
+                  <Separator className="my-4" />
+                  <div className="grid gap-4">
+                    <h4 className="text-sm font-medium">Suggestions</h4>
+                    <Button
+                      variant="secondary"
+                      className="rounded-full w-fit"
+                      onClick={() =>
+                        setInput('Show me the most popular events')
+                      }
+                    >
+                      Show me the most popular events
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="rounded-full w-fit"
+                      onClick={() =>
+                        setInput('Which event has the most attendees?')
+                      }
+                    >
+                      Which event has the most attendees?
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="rounded-full w-fit"
+                      onClick={() => setInput(`New Year's Eve Parties`)}
+                    >
+                      New Years Eve Parties
+                    </Button>
+                    <Button
+                      variant="secondary"
+                      className="rounded-full w-fit"
+                      onClick={() => setInput('Running clubs')}
+                    >
+                      Running clubs
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        <SheetFooter className="absolute bottom-0 flex flex-row w-full p-6 space-x-2">
+          {secondaryActionLabel && (
             <Button
+              variant="outline"
               type="button"
-              onClick={onSubmit}
               className="w-[50%]"
               size="lg"
+              onClick={step === STEPS.INPUT ? clearAll : onNext}
             >
-              {actionLabel}
+              {secondaryActionLabel}
             </Button>
-          </SheetFooter>
-        </SheetContent>
-      </Sheet>
-    </div>
+          )}
+          <Button
+            type="button"
+            onClick={onSubmit}
+            className="w-[50%]"
+            size="lg"
+          >
+            {actionLabel}
+          </Button>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   )
 }
