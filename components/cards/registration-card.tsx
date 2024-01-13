4e771804fc327e7574249ea88935348fcea7c999
justinkook <justinkook@gmail.com>
2024-01-13 00:09:07 -0500

    refactor(registration-card): dynamic props

--- components/cards/registration-card.tsx
+++ components/cards/registration-card.tsx
@@ -11,20 +11,33 @@ import {
 } from '@/components/ui/card'
 import { Counter } from '@/components/ui/counter'
 
-export function RegistrationCard() {
+type RegistrationCardProps = {
+  title?: string
+  price?: string
+  description?: string
+  buttonLabel?: string
+}
+
+export const RegistrationCard: React.FC<RegistrationCardProps> = ({
+  title = 'General Admission',
+  price = '$54',
+  description = 'GA Tickets for entrance A',
+  buttonLabel = 'Add to cart',
+}) => {
   return (
     <Card>
       <CardHeader className="pb-4">
         <CardTitle className="flex justify-between text-base">
-          <span>General Admission</span>$54
+          <span>{title}</span>
+          {price}
         </CardTitle>
-        <CardDescription>GA Tickets for entrance A</CardDescription>
+        <CardDescription>{description}</CardDescription>
       </CardHeader>
       <CardContent className="pb-2">
         <Counter />
       </CardContent>
       <CardFooter>
-        <Button className="w-full">Add to cart</Button>
+        <Button className="w-full">{buttonLabel}</Button>
       </CardFooter>
     </Card>
   )
