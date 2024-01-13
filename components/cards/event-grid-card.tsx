1aa489ea4826d7c5c2787926502e2b9467054808
justinkook <justinkook@gmail.com>
2024-01-13 00:45:46 -0500

    refactor(event-card): arrow func and clean code

--- components/cards/event-grid-card.tsx
+++ components/cards/event-grid-card.tsx
@@ -7,28 +7,57 @@ import { AvatarStack } from '@/components/ui/avatar-stack'
 import { Card, CardContent, CardFooter } from '@/components/ui/card'
 import { HeartButton } from '@/components/buttons/heart-button'
 
+// Mock data to be replaced later
+const mockData = {
+  name: 'Lena Logic',
+  description: 'Thinking Components',
+  eventLink: '/event/react-rendevous',
+  eventImage:
+    'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
+  location: '123 W 29 St, New York, NY',
+  date: 'Dec 08',
+  time: '7:00 PM',
+  avatars: [
+    '/images/card.png',
+    'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
+    'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
+    'https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80',
+  ],
+}
+
 type EventGridCardProps = React.HTMLAttributes<HTMLDivElement> & {
   name?: string
   description?: string
-  avatar?: string
-  followers?: number
-  createdAt?: string
+  eventLink?: string
+  eventImage?: string
+  location?: string
+  date?: string
+  time?: string
+  avatars?: string[]
 }
 
-export function EventGridCard({
+export const EventGridCard: React.FC<EventGridCardProps> = ({
   className,
+  name = mockData.name,
+  description = mockData.description,
+  eventLink = mockData.eventLink,
+  eventImage = mockData.eventImage,
+  location = mockData.location,
+  date = mockData.date,
+  time = mockData.time,
+  avatars = mockData.avatars,
   ...props
-}: React.HTMLAttributes<HTMLDivElement> & EventGridCardProps) {
+}) => {
   return (
     <Card className={cn(className)} {...props}>
       <CardContent className="pt-6">
         <div className="grid grid-cols-[1fr_auto] items-start gap-4 space-y-0">
           <div className="space-y-1">
             <div className="flex justify-start space-x-4">
-              <Link href={`/event/react-rendevous`}>
+              <Link href={eventLink}>
                 <div className="w-[80px] overflow-hidden rounded-lg">
                   <Image
-                    src="https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80"
+                    src={eventImage}
                     alt="event cover image"
                     width={80}
                     height={80}
@@ -37,12 +66,12 @@ export function EventGridCard({
                 </div>
               </Link>
               <div className="flex-col space-y-1">
-                <h4 className="text-sm font-semibold">Lena Logic</h4>
-                <p className="line-clamp-2 text-sm">Thinking Components</p>
+                <h4 className="text-sm font-semibold">{name}</h4>
+                <p className="line-clamp-2 text-sm">{description}</p>
                 <div className="flex items-center pt-2">
                   <MapPinIcon className="mr-2 h-4 w-4 opacity-70" />
                   <span className="text-xs text-muted-foreground">
-                    123 W 29 St, New York, NY
+                    {location}
                   </span>
                 </div>
               </div>
@@ -54,21 +83,14 @@ export function EventGridCard({
       <CardFooter className="flex space-x-4 text-sm text-muted-foreground">
         <div className="flex items-center">
           <CalendarDaysIcon className="mr-1 h-3 w-3" />
-          Dec 08
+          {date}
         </div>
         <div className="flex items-center">
           <Clock7Icon className="mr-1 h-3 w-3" />
-          7:00 PM
+          {time}
         </div>
         <div className="flex items-center">
-          <AvatarStack
-            avatars={[
-              '/images/card.png',
-              'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
-              'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
-              'https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80',
-            ]}
-          />
+          <AvatarStack avatars={avatars} />
         </div>
       </CardFooter>
     </Card>
