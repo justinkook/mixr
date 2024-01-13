5e71e16561612dc671b8be9bebebfae15f53a3eb
justinkook <justinkook@gmail.com>
2024-01-13 00:06:05 -0500

    refactor(appearance-form): theme option comp

--- components/forms/appearance/appearance-form.tsx
+++ components/forms/appearance/appearance-form.tsx
@@ -27,6 +27,23 @@ const appearanceFormSchema = z.object({
 
 type AppearanceFormValues = z.infer<typeof appearanceFormSchema>
 
+interface ThemeOptionProps {
+  value: 'light' | 'dark'
+  children: React.ReactNode
+}
+
+// Component to display theme option
+const ThemeOption: React.FC<ThemeOptionProps> = ({ value, children }) => (
+  <FormItem>
+    <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
+      <FormControl>
+        <RadioGroupItem value={value} className="sr-only" />
+      </FormControl>
+      {children}
+    </FormLabel>
+  </FormItem>
+)
+
 export function AppearanceForm() {
   const theme = useTheme()
   const form = useForm<AppearanceFormValues>({
@@ -56,58 +73,50 @@ export function AppearanceForm() {
                 defaultValue={field.value}
                 className="grid max-w-md grid-cols-2 gap-8 pt-2"
               >
-                <FormItem>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="light" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                      <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                        <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
+                {/* Light theme option */}
+                <ThemeOption value="light">
+                  <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
+                    <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
+                      <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
+                        <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
+                        <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                      </div>
+                      <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                        <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                        <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
+                      </div>
+                      <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
+                        <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
+                        <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
                       </div>
                     </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Light
-                    </span>
-                  </FormLabel>
-                </FormItem>
-                <FormItem>
-                  <FormLabel className="[&:has([data-state=checked])>div]:border-primary">
-                    <FormControl>
-                      <RadioGroupItem value="dark" className="sr-only" />
-                    </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                      <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                        <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
+                  </div>
+                  <span className="block w-full p-2 text-center font-normal">
+                    Light
+                  </span>
+                </ThemeOption>
+                {/* Dark theme option */}
+                <ThemeOption value="dark">
+                  <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
+                    <div className="space-y-2 rounded-sm bg-slate-950 p-2">
+                      <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                        <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
+                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                      </div>
+                      <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                        <div className="h-4 w-4 rounded-full bg-slate-400" />
+                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
+                      </div>
+                      <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
+                        <div className="h-4 w-4 rounded-full bg-slate-400" />
+                        <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
                       </div>
                     </div>
-                    <span className="block w-full p-2 text-center font-normal">
-                      Dark
-                    </span>
-                  </FormLabel>
-                </FormItem>
+                  </div>
+                  <span className="block w-full p-2 text-center font-normal">
+                    Dark
+                  </span>
+                </ThemeOption>
               </RadioGroup>
             </FormItem>
           )}
