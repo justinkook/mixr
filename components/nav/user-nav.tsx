cb4f807c048e965548218e4791972789088d89f9
justinkook <justinkook@gmail.com>
2024-01-13 20:06:04 -0500

    revert(user-nav): hide on mobile, remove drawer

--- components/nav/user-nav.tsx
+++ components/nav/user-nav.tsx
@@ -5,19 +5,8 @@ import { SignOutButton, useUser } from '@clerk/nextjs'
 import { LogOutIcon } from 'lucide-react'
 
 import { userNav } from '@/config/nav'
-import { useMediaQuery } from '@/hooks/use-media-query'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -30,61 +19,10 @@ import {
 
 export const UserNav: React.FC = () => {
   const { user } = useUser()
-  const isDesktop = useMediaQuery('(min-width: 768px)')
 
-  if (isDesktop) {
-    return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="ghost"
-            className="relative h-8 w-8 rounded-full hiddden sm:flex pl-4"
-          >
-            <Avatar className="h-8 w-8">
-              <AvatarImage src={user?.imageUrl} alt="Profile Avatar" />
-              <AvatarFallback>
-                {user?.username?.slice(0, 2).toUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
-                {user?.fullName}
-              </p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {user?.emailAddresses[0].emailAddress}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            {userNav.map((navItem) => (
-              <Link key={navItem.href} href={navItem.href}>
-                <DropdownMenuItem>
-                  {navItem.icon && <navItem.icon className="mr-2 w-4 h-4" />}
-                  <span>{navItem.title}</span>
-                </DropdownMenuItem>
-              </Link>
-            ))}
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-
-          <SignOutButton>
-            <DropdownMenuItem>
-              <LogOutIcon className="mr-2 h-4 w-4" />
-              <span>Log out</span>
-            </DropdownMenuItem>
-          </SignOutButton>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    )
-  }
   return (
-    <Drawer>
-      <DrawerTrigger asChild>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
           className="relative h-8 w-8 rounded-full hidden sm:flex pl-4"
@@ -92,39 +30,39 @@ export const UserNav: React.FC = () => {
           <Avatar className="h-8 w-8">
             <AvatarImage src={user?.imageUrl} alt="Profile Avatar" />
             <AvatarFallback>
-              {user?.fullName?.slice(0, 2).toUpperCase()}
+              {user?.username?.slice(0, 2).toUpperCase()}
             </AvatarFallback>
           </Avatar>
         </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{user?.fullName}</DrawerTitle>
-          <DrawerDescription>
-            {user?.emailAddresses[0].emailAddress}
-          </DrawerDescription>
-        </DrawerHeader>
-        <div className="space-y-2 flex flex-col px-4">
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{user?.fullName}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {user?.emailAddresses[0].emailAddress}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
           {userNav.map((navItem) => (
-            <Button key={navItem.href} variant="secondary" asChild>
-              <Link key={navItem.href} href={navItem.href}>
-                <div className="pl-4 relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50">
-                  <span>{navItem.title}</span>
-                </div>
-              </Link>
-            </Button>
+            <Link key={navItem.href} href={navItem.href}>
+              <DropdownMenuItem>
+                {navItem.icon && <navItem.icon className="mr-2 w-4 h-4" />}
+                <span>{navItem.title}</span>
+              </DropdownMenuItem>
+            </Link>
           ))}
-        </div>
-        <DrawerFooter>
-          <DrawerClose asChild>
-            <SignOutButton>
-              <Button variant="outline" className="w-full">
-                Log out
-              </Button>
-            </SignOutButton>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <SignOutButton>
+          <DropdownMenuItem>
+            <LogOutIcon className="mr-2 h-4 w-4" />
+            <span>Log out</span>
+          </DropdownMenuItem>
+        </SignOutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
